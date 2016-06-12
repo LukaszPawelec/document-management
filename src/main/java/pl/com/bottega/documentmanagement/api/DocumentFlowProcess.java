833348f9d45b41e477dfdacc489753abbc15a216
Nizari <nizari@o2.pl>
2016-06-12 15:31:47 +0200

    checkNotNull added

--- src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
+++ src/main/java/pl/com/bottega/documentmanagement/api/DocumentFlowProcess.java
@@ -2,32 +2,45 @@ package pl.com.bottega.documentmanagement.api;
 
 import pl.com.bottega.documentmanagement.domain.DocumentNumber;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by Nizari on 12.06.2016.
  */
 public class DocumentFlowProcess {
 
     public DocumentNumber create(String title, String content) {
+        checkNotNull(title);
+        checkNotNull(content);
+
         return null;
     }
 
     public void change(DocumentNumber documentNumber, String newTitle, String newContent) {
+        checkNotNull(documentNumber);
+        checkNotNull(newTitle);
+        checkNotNull(newContent);
 
     }
 
     public void verify(DocumentNumber documentNumber) {
+        checkNotNull(documentNumber);
 
     }
 
     public void publish(DocumentNumber documentNumber) {
+        checkNotNull(documentNumber);
 
     }
 
     public void archive(DocumentNumber documentNumber) {
+        checkNotNull(documentNumber);
 
     }
 
     public DocumentNumber createNewVersion(DocumentNumber documentNumber) {
+        checkNotNull(documentNumber);
+
         return null;
     }
 }
